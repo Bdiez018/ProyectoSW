c7f0660539f55437c873add149c1609053a7f7e2
borja <borjadiezgonzalez@gmail.com>
2015-10-01 11:55:11 +0200

    sqlphphtml

--- Registrar.php
+++ Registrar.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
-	mysql_connect("mysql.hostinger.es","u998980514_God","qwerty") or die(mysql_error());
+	mysql_connect("mysql.hostinger.es","u998980514_god","sistemasweb") or die(mysql_error());
 	mysql_select_db("u998980514_quiz") or die(mysql_error());
 	$sql="INSERT INTO usuario(nombre,email,pass,telefono,especialidad,interes) VALUES ('$_POST[nameyapp]','$_POST[correo]','$_POST[password]','$_POST[tel]','$_POST[espec]','$_POST[interes]')";
 	if(!mysql_query($sql)){
