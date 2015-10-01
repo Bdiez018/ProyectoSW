0e9270086d76a1b9044ae7c98b007bdb0e78c1c9
borja <borjadiezgonzalez@gmail.com>
2015-10-01 11:58:13 +0200

    sqlphphtml

--- Registrar.php
+++ Registrar.php
@@ -1,16 +1,17 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
-	mysql_connect("mysql.hostinger.es","u998980514_god","sistemasweb") or die(mysql_error());
-	mysql_select_db("u998980514_quiz") or die(mysql_error());
+	mysqli_connect("mysql.hostinger.es","u998980514_god","sistemasweb") or die(mysqli_error());
+	mysqli_select_db("u998980514_quiz") or die(mysqli_error());
 	$sql="INSERT INTO usuario(nombre,email,pass,telefono,especialidad,interes) VALUES ('$_POST[nameyapp]','$_POST[correo]','$_POST[password]','$_POST[tel]','$_POST[espec]','$_POST[interes]')";
-	if(!mysql_query($sql)){
-		die('Error:' .mysql_error());
+	if(!mysqli_query($sql)){
+		die('Error:' .mysqli_error());
 	}
 	echo "1 registro añadido";
 	mysql_close();
 ?>
 <html>
 <body>
+<BR>
 <a href="VerUsuarios.php">Ver usuarios de la BD</a>';  
 </body>
 </html>
