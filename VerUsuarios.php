cc39fd1c5912cc34cbc8ceee336cd7008fa2cecc
borja <borjadiezgonzalez@gmail.com>
2015-10-01 18:27:29 +0200

    verfoto

---
+++ VerUsuarios.php
@@ -0,0 +1,12 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+	mysql_connect("lo","root","") or die(mysql_error());
+	mysql_select_db("pruebanombre") or die(mysql_error());
+	echo'<table border=1 align="center"> <tr> <th>Nombre</th><th>Correo</th><th>Telefono</th><th>Especialidad</th></tr>';
+	$sql=mysql_query("SELECT * from usuario");
+	while($linea=mysql_fetch_array($sql)){
+		echo'<tr><td>'.$linea['nombre'].'</td><td>'.$linea['email'].'</td><td>'.$linea['telefono'].'</td><<td>'.$linea['especialidad'].'</td>/tr>';
+	}
+	echo'</table>';
+
+?>
