1da734733afceb7f2525a7f7cec55896a146f345
borja <borjadiezgonzalez@gmail.com>
2015-10-02 16:11:58 +0200

    verElem

--- Registrar.php
+++ Registrar.php
@@ -3,8 +3,8 @@
 	mysql_connect("mysql.hostinger.es","u998980514_god","sistemasweb") or die(mysql_error());
 	mysql_select_db("u998980514_quiz") or die(mysql_error());
 	$sql="INSERT INTO usuario(nombre,email,pass,telefono,especialidad,interes) VALUES ('$_POST[nameyapp]','$_POST[correo]','$_POST[password]','$_POST[tel]','$_POST[espec]','$_POST[interes]')";
-	if(!mysqli_query($sql)){
-		die('Error:' .mysqli_error());
+	if(!mysql_query($sql)){
+		die('Error:' .mysql_error());
 	}
 	echo "1 registro añadido";
 	mysql_close();
--- VerUsuarios.php
+++ VerUsuarios.php
@@ -1,11 +1,11 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
-	mysql_connect("lo","root","") or die(mysql_error());
-	mysql_select_db("pruebanombre") or die(mysql_error());
-	echo'<table border=1 align="center"> <tr> <th>Nombre</th><th>Correo</th><th>Telefono</th><th>Especialidad</th></tr>';
+	mysql_connect("mysql.hostinger.es","u998980514_god","sistemasweb") or die(mysql_error());
+	mysql_select_db("u998980514_quiz") or die(mysql_error());
+	echo'<table border=1 align="center"> <tr><th>Nombre</th><th>Correo</th><th>Telefono</th><th>Especialidad</th></tr>';
 	$sql=mysql_query("SELECT * from usuario");
 	while($linea=mysql_fetch_array($sql)){
-		echo'<tr><td>'.$linea['nombre'].'</td><td>'.$linea['email'].'</td><td>'.$linea['telefono'].'</td><<td>'.$linea['especialidad'].'</td>/tr>';
+		echo'<tr><td>'.$linea['nombre'].'</td><td>'.$linea['email'].'</td><td>'.$linea['telefono'].'</td><<td>'.$linea['especialidad'].'</td></tr>';
 	}
 	echo'</table>';
 
